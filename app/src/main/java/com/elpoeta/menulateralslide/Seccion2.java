1de461ddeda2157e199768ede257442c90d58f20
ramiromadriaga <ramiro.madriaga@gmail.com>
2014-08-26 08:55:24 -0300

    Menu Lateral Deslizable Slide
    es una menu lateral personalizado

---
+++ app/src/main/java/com/elpoeta/menulateralslide/Seccion2.java
@@ -0,0 +1,22 @@
+package com.elpoeta.menulateralslide;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 26/08/2014.
+ */
+public class Seccion2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.seccion2, container, false);
+
+        return rootView;
+    }
+}
